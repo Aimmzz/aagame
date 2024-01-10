d8c14b63c16422ee84231ab81422d2f4394e0b40
Aimmzz <rohimkurniawan456@gmail.com>
2024-01-10 15:18:45 +0700

    Initial Commit

---
+++ AAGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AAGame
+//
+//  Created by rohim kurniawan on 10/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
